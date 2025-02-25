374119d3fa140c0395d693577998120388a5b6af
Saurav11292 <111349821+Saurav11292@users.noreply.github.com>
2025-02-25 21:42:44 +0530

    changed typo

--- Datatactic/src/ContactSection/Contacts.tsx
+++ Datatactic/src/ContactSection/Contacts.tsx
@@ -6,7 +6,7 @@ export default function Contacts() {
          <section className="p-8 flex items-center justify-center gap-10 bg-gray-800">
         <h2 className="text-3xl text-gray-400 font-semibold">Take the first step toward a smarter future with POI data!</h2>
         <button type="submit" className="p-4 text-gray-200 bg-blue-400 hover:bg-blue-700 rounded-lg">
-            Request data sample
+            Request a demo
           </button>
       </section>
     </div>
